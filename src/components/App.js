50c83de46fa68be02b16d13edc40f3e433841513
YuraFLex <shvalov.yurec@gmail.com>
2022-10-24 07:18:14 +0300

    add hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,20 @@
-import { Component } from "react";
-import { Box } from "../constans";
-import FeedBack from "./FeedBackOptions/FeedbackOptions";
-import Statistics from "./Statistics/Statistics";
-import Notification from "./Notification/Notification";
+import { useState } from 'react';
+import { Box } from '../constans';
+import FeedBack from './FeedBackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const allFeedback = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const allFeedback = countTotalFeedback();
     let result = 0;
     if (allFeedback > 0) {
       result = Math.round((good * 100) / allFeedback);
@@ -26,42 +22,52 @@ export class App extends Component {
     return result;
   };
 
-  feedbaclLeaveClick = (event) => {
-    const feedbackCount = event.target.name;
+  const feedbaclLeaveClick = event => {
+    const option = event.target.name;
 
-    this.setState((prevState) => ({
-      [feedbackCount]: prevState[feedbackCount] + 1,
-    }));
+    switch (option) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const btnName = Object.keys(this.state);
-    const feedbackTotal = this.countTotalFeedback();
-    const positiveFeedback = this.countPositiveFeedbackPercentage();
-    return (
-      <Box as="main">
-        <Box as="section" display="flex" justifyContent="center" py={5}>
-          <FeedBack
-            title={"Please leave feedback"}
-            btnName={btnName}
-            onLeaveFeedback={this.feedbaclLeaveClick}
-          />
-        </Box>
-        <Box as="section" display="flex" justifyContent="center" py={4}>
-          {feedbackTotal === 0 ? (
-            <Notification message={"There is no feedback"} />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={feedbackTotal}
-              positivePercentage={positiveFeedback}
-            />
-          )}
-        </Box>
+  const btnName = ['good', 'neutral', 'bad'];
+  const feedbackTotal = countTotalFeedback();
+  const positiveFeedback = countPositiveFeedbackPercentage();
+  return (
+    <Box as="main">
+      <Box as="section" display="flex" justifyContent="center" py={5}>
+        <FeedBack
+          title={'Please leave feedback'}
+          btnName={btnName}
+          onLeaveFeedback={feedbaclLeaveClick}
+        />
       </Box>
-    );
-  }
-}
+      <Box as="section" display="flex" justifyContent="center" py={4}>
+        {feedbackTotal === 0 ? (
+          <Notification message={'There is no feedback'} />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={feedbackTotal}
+            positivePercentage={positiveFeedback}
+          />
+        )}
+      </Box>
+    </Box>
+  );
+};
